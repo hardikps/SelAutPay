3bd13dd3a9586ebfaced7473737cdd4f051be91c
hardikps <hardikpawar123@outlook.com>
2022-03-16 11:36:53 +0530

    first commit

--- main.py
+++ main.py
@@ -2,12 +2,24 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.Chrome(executable_path="D:\\chromedriver_win32\\chromedriver.exe")
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+browser = webdriver.Chrome(executable_path="C:\\Users\\hardik\\Desktop\\chromedriver_win32\\chromedriver.exe")
 
 browser.get('https://apps.paybooks.in/')
 assert 'Paybooks Login | Paybooks App | Paybooks Admin Login' in browser.title
 
-elem = browser.find_element(By.NAME, 'p')  # Find the search box
-elem.send_keys('seleniumhq' + Keys.RETURN)
+inputElement = browser.find_element(By.NAME, 'txtUserName')  # Find the search box
+inputElement.send_keys('279')
 
-browser.quit()
+inputElement = browser.find_element(By.NAME, 'txtPassword')  # Find the search box
+inputElement.send_keys('H@rdikp098')
+
+inputElement = browser.find_element(By.NAME, 'txtDomain')  # Find the search box
+inputElement.send_keys('ACC')
+
+inputElement.send_keys(Keys.ENTER)
+
+inputElement = browser.find_element(By.CLASS_NAME, 'ng-scope')
+inputElement.
